cde80b5bddcc5df45c532cc492754d90d7797c89
lha901222 <lha901222@gmail.com>
2024-05-18 06:09:52 +0900

    feat : header 자바스크립트 작업 완료

--- public/www/js/header.js
+++ public/www/js/header.js
@@ -1,18 +1,47 @@
+// 모바일 메뉴 반응형
 window.addEventListener("load", function () {
   const header = document.querySelector(".header");
-  const headerNav = document.querySelector(".header-nav");
-  let lastScrollY = window.scrollY;
+  // const inner = document.querySelector(".inner");
+  // const headerNav = document.querySelector(".header-nav");
 
-  window.addEventListener("scroll", function () {
-    if (window.scrollY > lastScrollY && window.scrollY > 100) {
-      // 스크롤 다운: 전체 헤더를 숨기고 네비게이션만 보이게 함
-      header.classList.add("hidden");
-      headerNav.style.display = "block";
+  function handleScroll() {
+    if (window.scrollY === 0) {
+      header.classList.remove("scroll");
+      // inner.classList.remove("inner-scroll");
+      // headerNav.classList.remove("nav-scroll");
     } else {
-      // 스크롤 업: 전체 헤더를 보이게 함
-      header.classList.remove("hidden");
-      headerNav.style.display = "";
+      header.classList.add("scroll");
+      // inner.classList.add("inner-scroll");
+      // headerNav.classList.add("nav-scroll");
     }
-    lastScrollY = window.scrollY;
+  }
+
+  handleScroll();
+
+  window.addEventListener("scroll", handleScroll);
+});
+
+// 메뉴 클릭 시 언더바
+window.addEventListener("load", function () {
+  const navLinks = document.querySelectorAll(".header-nav .nav-list li a");
+
+  function addUnderline() {
+    navLinks.forEach(function (link) {
+      link.classList.remove("underline");
+    });
+    navLinks[0].classList.add("underline");
+  }
+
+  addUnderline();
+
+  function handleNavClick(event) {
+    navLinks.forEach(function (link) {
+      link.classList.remove("underline");
+    });
+    event.target.classList.add("underline");
+  }
+
+  navLinks.forEach(function (link) {
+    link.addEventListener("click", handleNavClick);
   });
 });
