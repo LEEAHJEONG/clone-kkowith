618f70097e2b85ef609296a1019e5092a530bb1f
lha901222 <lha901222@gmail.com>
2024-05-20 02:03:24 +0900

    feat : header 클릭이벤트 작업중

--- public/www/js/header.js
+++ public/www/js/header.js
@@ -1,26 +1,24 @@
 // 모바일 메뉴 반응형
 window.addEventListener("load", function () {
   const header = document.querySelector(".header");
-  // const inner = document.querySelector(".inner");
-  // const headerNav = document.querySelector(".header-nav");
+  const headerNav = document.querySelector(".header-nav");
 
-  function handleScroll() {
+  function moveScroll() {
     if (window.scrollY === 0) {
       header.classList.remove("scroll");
-      // inner.classList.remove("inner-scroll");
-      // headerNav.classList.remove("nav-scroll");
+      headerNav.classList.remove("nav-scroll");
     } else {
       header.classList.add("scroll");
-      // inner.classList.add("inner-scroll");
-      // headerNav.classList.add("nav-scroll");
+      headerNav.classList.add("nav-scroll");
     }
   }
 
-  handleScroll();
+  moveScroll();
 
-  window.addEventListener("scroll", handleScroll);
+  window.addEventListener("scroll", moveScroll);
 });
 
+// 메뉴 클릭 시 언더바
 // 메뉴 클릭 시 언더바
 window.addEventListener("load", function () {
   const navLinks = document.querySelectorAll(".header-nav .nav-list li a");
